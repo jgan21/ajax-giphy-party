8697ea7780be32c301a2502d161e643c98bfe234
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-14 19:34:21 -0600

    final touches pre-code review

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
 "use strict";
 
 
-$("#search-button").on("click", getGiphyForSearchAndAddImage);
-$("#remove-images-button").on("click", removeAllImages);
 
 
 /** Use form entry to search on giphy api. No input,
  * Add the image of first search to the DOM
 */
 
-async function getGiphyForSearchAndAddImage(evt){
+async function getGiphyForSearchAndAddImage(evt) {
   evt.preventDefault();
 
   const searchInput = $("input").val();
   const params = new URLSearchParams({
-      q:searchInput, api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"})
+    q: searchInput, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  });
 
   const response = await fetch(
     `https://api.giphy.com/v1/gifs/search?${params}`,
-      {method: "GET",
+    {
+      method: "GET",
       headers: {
-        "Content-Type": "application/json"}
-      });
+        "Content-Type": "application/json"
+      }
+    });
+
   const searchResult = await response.json();
-
+  addImage(searchResult);
   // console.log("giphyResponse=", response, "json=", searchResult);
+}
 
+
+/** Taking in image result from server and adding it onto the DOM */
+
+function addImage(searchResult) {
   // get first image result from response
-  const firstImageResult = searchResult.data[0].images.fixed_height.url
+  const firstImageResult = searchResult.data[0].images.fixed_height.url;
   console.log('first image result:', firstImageResult);
 
   // add image to DOM
   const $newImage = $('<img>').attr('src', firstImageResult);
   $('#images').append($newImage);
-
 }
+
+/** Remove all images from the section */
 
 function removeAllImages(evt) {
   evt.preventDefault();
+  $('section').empty();
 }
 
-
-
 console.log("Let's get this party started!");
+$("#search-button").on("click", getGiphyForSearchAndAddImage);
+$("#remove-images-button").on("click", removeAllImages);
