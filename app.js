8d9b067e33626b2537cbc34f4174b6df44e18e91
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-14 18:56:31 -0600

    added javascript to get gifs

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+"use strict";
+
+
+$("button").on("click", getGiphyForSearch);
+
+async function getGiphyForSearch(evt){
+  evt.preventDefault();
+
+  const searchInput = $("input").val();
+  const params = new URLSearchParams({
+      q:searchInput, api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"})
+
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?${params}`,
+      {method: "GET",
+      headers: {
+        "Content-Type": "application/json"}
+      });
+  const searchResult = await response.text();
+
+  console.log("giphyResponse=", response, "text=", searchResult);
+}
+
+// https://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+
 console.log("Let's get this party started!");
