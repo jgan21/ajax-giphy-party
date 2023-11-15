da4be7b1fefc25b89bb3ea71706275f463e2a386
Jia Rong Gan <jiaronggan21@gmail.com>
2023-11-14 20:02:45 -0600

    refactor code

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 "use strict";
 
 
-
-
 /** Use form entry to search on giphy api. No input,
- * Add the image of first search to the DOM
+ * return the searched image.
 */
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const GIPHY_URL = "https://api.giphy.com/v1/gifs";
 
-async function getGiphyForSearchAndAddImage(evt) {
-  evt.preventDefault();
-
+async function getGiphyFromSearch() {
+  console.log("started");
   const searchInput = $("input").val();
   const params = new URLSearchParams({
-    q: searchInput, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    q: searchInput, api_key: API_KEY
   });
 
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?${params}`,
+    `${GIPHY_URL}/search?${params}`,
     {
       method: "GET",
       headers: {
@@ -25,17 +24,16 @@ async function getGiphyForSearchAndAddImage(evt) {
     });
 
   const searchResult = await response.json();
-  addImage(searchResult);
-  // console.log("giphyResponse=", response, "json=", searchResult);
+  // get first image result from response
+  const firstImageResult = searchResult.data[0].images.fixed_height.url;
+  console.log('first image result:', firstImageResult);
+  return firstImageResult;
 }
 
 
 /** Taking in image result from server and adding it onto the DOM */
 
-function addImage(searchResult) {
-  // get first image result from response
-  const firstImageResult = searchResult.data[0].images.fixed_height.url;
-  console.log('first image result:', firstImageResult);
+function addImage(firstImageResult) {
 
   // add image to DOM
   const $newImage = $('<img>').attr('src', firstImageResult);
@@ -44,11 +42,21 @@ function addImage(searchResult) {
 
 /** Remove all images from the section */
 
-function removeAllImages(evt) {
-  evt.preventDefault();
+function removeAllImages() {
   $('section').empty();
 }
 
+/** Conductor function - will began running the gif search */
+
+async function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const firstImageResult = await getGiphyFromSearch();
+  console.log("image=", firstImageResult);
+
+  addImage(firstImageResult);
+}
+
 console.log("Let's get this party started!");
-$("#search-button").on("click", getGiphyForSearchAndAddImage);
+$("#search-button").on("click", handleSubmit);
 $("#remove-images-button").on("click", removeAllImages);
